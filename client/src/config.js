e2e9562e89b33edd201e3acabe230e6215dbb0e8
Chandan Kumar <chandankumar835205gmail@gmail.com>
2026-09-03 19:31:32 +0530

    feat: update client server url with production render backend

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,12 @@
+// export const SERVER_URL = import.meta.env.VITE_SERVER_URL || (
+//   typeof window !== 'undefined' && window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1'
+//     ? `${window.location.protocol}//${window.location.hostname}:5000`
+//     : 'http://localhost:5000'
+// );
+
+
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL || (
   typeof window !== 'undefined' && window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1'
-    ? `${window.location.protocol}//${window.location.hostname}:5000`
+    ? 'https://kluff.onrender.com'
     : 'http://localhost:5000'
 );
